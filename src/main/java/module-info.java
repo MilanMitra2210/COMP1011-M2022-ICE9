773c25ee5b25624d3ab7d78eed2b7296a3662f87
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-07-21 19:29:13 -0400

    added APIManager v1

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module ca.georgiancollege.comp1011m2022ice9 {
     requires javafx.fxml;
     requires java.sql;
     requires com.google.gson;
+    requires java.net.http;
 
     opens ca.georgiancollege.comp1011m2022ice9 to javafx.fxml, com.google.gson;
     exports ca.georgiancollege.comp1011m2022ice9;
